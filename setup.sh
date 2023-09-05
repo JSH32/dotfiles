bf8cea3f5bbf6a6f621c22954c89228ac172c89c
JSH32 <rudnik7000@gmail.com>
2023-09-05 02:08:27 -0400

    Fix setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@ if [ ! -f "/etc/arch-release" ]; then
 fi
 
 # Installing yay for downloading other packages
-if [ ! command -v yay &> /dev/null ]; then
+if ! command -v yay &> /dev/null
+then
     echo "Installing yay"
     pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 fi
@@ -19,7 +20,8 @@ git submodule update --init --recursive
 echo "Making zsh the default shell"
 sudo chsh -s $(which zsh)
 
-if [ ! command -v oh-my-posh &> /dev/null ]; then
+if ! command -v oh-my-posh &> /dev/null
+then
   echo "Installing Oh-My-Posh"
   curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 fi
