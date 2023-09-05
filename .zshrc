2190368f9b0227a06483cc25dc5a63f4aa407bfc
JSH32 <rudnik7000@gmail.com>
2023-09-05 02:05:08 -0400

    Create install steps and add more configs

---
+++ .zshrc
@@ -0,0 +1,7 @@
+source ~/.config/zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.config/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+eval "$(oh-my-posh init zsh --config $HOME/.config/zsh/theme.omp.json)"
+
+alias ls='exa --icons --group-directories-first'
+alias cat='bat'
---
+++ setup.sh
@@ -0,0 +1,27 @@
+if [ ! -f "/etc/arch-release" ]; then
+  echo "This script only works on Arch derivatives"
+  exit 1
+fi
+
+# Installing yay for downloading other packages
+if [ ! command -v yay &> /dev/null ]; then
+    echo "Installing yay"
+    pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+fi
+
+# Install required dependencies
+yay -S --needed bspwm picom rofi sxhkd dunst zsh xfce4-screensaver xfce4-notifyd xfce4-power-manager \
+                network-manager-applet flameshot ttf-jetbrains-mono-nerd pavucontrol brightnessctl blueman dotbot
+
+echo "Cloning submodules"
+git submodule update --init --recursive
+
+echo "Making zsh the default shell"
+sudo chsh -s $(which zsh)
+
+if [ ! command -v oh-my-posh &> /dev/null ]; then
+  echo "Installing Oh-My-Posh"
+  curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+fi
+
+dotbot -c ./install.conf.yaml
